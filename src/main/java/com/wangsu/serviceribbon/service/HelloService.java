2107b3226ca1dcdca577498949dfb6dced730103
Hodor2333 <1749327912@qq.com>
2019-03-14 15:18:31 +0800

    add hystrix

--- src/main/java/com/wangsu/serviceribbon/service/HelloService.java
+++ src/main/java/com/wangsu/serviceribbon/service/HelloService.java
@@ -1,6 +1,7 @@
 package com.wangsu.serviceribbon.service;
 
-//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,13 +12,13 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
-//    @HystrixCommand(fallbackMethod = "hiError")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://EUREKA-SERVICE/hi?name="+name, String.class);
     }
 
-//    public String hiError(String name) {
-//        return "hi,"+name+",sorry,error!";
-//    }
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
 
 }
